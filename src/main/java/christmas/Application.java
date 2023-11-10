bd35d996545daaa11753dac97d9c48402163aaee
robinjoon <robin980108@naver.com>
2023-11-11 08:01:37 +0900

    feat(재시도 기능) : 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,55 +1,13 @@
 package christmas;
 
 import christmas.domain.badge.BadgeCalculator;
-import christmas.domain.menu.SelectedMenus;
-import christmas.domain.statistics.Statistics;
+import christmas.domain.eventplanner.EventPlanner;
 import christmas.domain.statistics.StatisticsGenerator;
-import christmas.exception.DuplicateMenuNameException;
-import christmas.exception.MenuSelectedCountOverflowException;
-import christmas.exception.NoSuchDateException;
-import christmas.exception.NoSuchMenuException;
-import christmas.exception.OnlyBeverageSelectedException;
-import christmas.exception.UnableToSplitByBarException;
-import christmas.exception.UnableToSplitByCommaException;
-import christmas.input.InputManager;
-import christmas.output.OutputManager;
-import java.time.LocalDate;
-import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        OutputManager.printStartMessage();
-        LocalDate selectedDate;
-        while (true) {
-            try {
-                selectedDate = InputManager.getDateFromConsole();
-                break;
-            } catch (NoSuchDateException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        OutputManager.printMenuInputGuide();
-        Map<String, Integer> menuNameToSelectedCount;
-
-        while (true) {
-            try {
-                menuNameToSelectedCount = InputManager.getMenuNameToSelectedCountMapFromConsole();
-                break;
-            } catch (UnableToSplitByBarException | UnableToSplitByCommaException | DuplicateMenuNameException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        SelectedMenus selectedMenus;
-        while (true) {
-            try {
-                selectedMenus = new SelectedMenus(menuNameToSelectedCount);
-                break;
-            } catch (OnlyBeverageSelectedException | MenuSelectedCountOverflowException | NoSuchMenuException e) {
-                System.out.println(e.getMessage());
-            }
-        }
         StatisticsGenerator statisticsGenerator = new StatisticsGenerator(new BadgeCalculator());
-        Statistics statistics = statisticsGenerator.makeStatistics(selectedDate, selectedMenus);
-        OutputManager.print(statistics);
+        EventPlanner eventPlanner = new EventPlanner(statisticsGenerator);
+        eventPlanner.start();
     }
 }
---
+++ src/main/java/christmas/domain/eventplanner/EventPlanner.java
@@ -0,0 +1,47 @@
+package christmas.domain.eventplanner;
+
+import christmas.domain.badge.BadgeCalculator;
+import christmas.domain.menu.SelectedMenus;
+import christmas.domain.statistics.Statistics;
+import christmas.domain.statistics.StatisticsGenerator;
+import christmas.input.InputManager;
+import christmas.output.OutputManager;
+import java.time.LocalDate;
+import java.util.Map;
+
+public final class EventPlanner {
+    private final StatisticsGenerator statisticsGenerator;
+
+    public EventPlanner(StatisticsGenerator statisticsGenerator) {
+        this.statisticsGenerator = statisticsGenerator;
+    }
+
+    public void start() {
+        LocalDate selectedDate = getSelectedDate();
+        SelectedMenus selectedMenus = getSelectedMenus();
+        StatisticsGenerator statisticsGenerator = new StatisticsGenerator(new BadgeCalculator());
+        Statistics statistics = statisticsGenerator.makeStatistics(selectedDate, selectedMenus);
+        OutputManager.print(statistics);
+    }
+
+    private SelectedMenus getSelectedMenus() {
+        RetryHelper<SelectedMenus> selectedMenusretryHelper = new RetryHelper<>();
+        return selectedMenusretryHelper.retry(this::getSelectedMenusOnce);
+    }
+
+    private SelectedMenus getSelectedMenusOnce() {
+        OutputManager.printMenuInputGuide();
+        Map<String, Integer> menuNameToSelectedCount = InputManager.getMenuNameToSelectedCountMapFromConsole();
+        return new SelectedMenus(menuNameToSelectedCount);
+    }
+
+    private LocalDate getSelectedDate() {
+        RetryHelper<LocalDate> localDateRetryHelper = new RetryHelper<>();
+        return localDateRetryHelper.retry(this::getLocalDateOnce);
+    }
+
+    private LocalDate getLocalDateOnce() {
+        OutputManager.printStartMessage();
+        return InputManager.getDateFromConsole();
+    }
+}
