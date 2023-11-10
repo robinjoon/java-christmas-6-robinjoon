25f08eb48beae1863d3faca0c391cb878518b86c
robinjoon <robin980108@naver.com>
2023-11-11 04:37:52 +0900

    feat(선택된 메뉴 생성 기능 - 총 선택 개수 제한) : 기능 구현

--- src/main/java/christmas/domain/menu/SelectedMenus.java
+++ src/main/java/christmas/domain/menu/SelectedMenus.java
@@ -2,18 +2,43 @@ package christmas.domain.menu;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class SelectedMenus {
     private final List<SelectedMenu> selectedMenus;
 
     public SelectedMenus(final Map<String, Integer> menuNameToSelectedCount) {
+        validate(menuNameToSelectedCount);
         selectedMenus = menuNameToSelectedCount.keySet()
                 .stream()
-                .map(menuName -> {
-                    Menu menu = Menu.from(menuName);
-                    return new SelectedMenu(menu, menuNameToSelectedCount.get(menuName));
-                })
+                .map(menuName -> selectMenu(menuNameToSelectedCount, menuName))
                 .toList();
+    }
+
+    private static SelectedMenu selectMenu(Map<String, Integer> menuNameToSelectedCount, String menuName) {
+        Menu menu = Menu.from(menuName);
+        return new SelectedMenu(menu, menuNameToSelectedCount.get(menuName));
+    }
+
+    private void validate(final Map<String, Integer> menuNameToSelectedCount) {
+        validateUnknownMenuName(menuNameToSelectedCount);
+        validateTotalMenuCount(menuNameToSelectedCount);
+    }
+
+    private void validateUnknownMenuName(final Map<String, Integer> menuNameToSelectedCount) {
+        Set<String> menuNames = menuNameToSelectedCount.keySet();
+        menuNames.forEach(Menu::from);
+    }
+
+    private void validateTotalMenuCount(final Map<String, Integer> menuNameToSelectedCount) {
+        int totalMenuCount = calculateTotalMenuCount(menuNameToSelectedCount);
+        if (totalMenuCount > 20) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static int calculateTotalMenuCount(Map<String, Integer> menuNameToSelectedCount) {
+        return menuNameToSelectedCount.values().stream().mapToInt(value -> value).sum();
     }
 
     public int getTotalPrice() {
--- src/test/java/christmas/domain/menu/SelectedMenusTest.java
+++ src/test/java/christmas/domain/menu/SelectedMenusTest.java
@@ -3,6 +3,7 @@ package christmas.domain.menu;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,6 +14,13 @@ class SelectedMenusTest {
     @ValueSource(strings = {"aaa", "bbb", "cxccc", "크림수프"})
     void unKnownMenu(String menuName) {
         Assertions.assertThatThrownBy(() -> new SelectedMenus(Map.of(menuName, 1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("총 주문 개수 초과")
+    @Test
+    void totalMenuCountOverFlow() {
+        Assertions.assertThatThrownBy(() -> new SelectedMenus(Map.of("양송이수프", 21)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
