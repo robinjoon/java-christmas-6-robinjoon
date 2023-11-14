673798effaf6cf599b0fab765ec2499d6011eb3a
robinjoon <robin980108@naver.com>
2023-11-14 23:32:14 +0900

    style(진행 기능) : 시작 메세지 문자열 블록을 활용하도록 변경

--- src/main/java/christmas/output/OutputManager.java
+++ src/main/java/christmas/output/OutputManager.java
@@ -13,8 +13,10 @@ import java.time.LocalDate;
 public final class OutputManager {
 
     public static void printStartMessage() {
-        System.out.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
-                + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n");
+        System.out.print("""
+                안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.
+                12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)
+                """);
     }
 
     public static void printMenuInputGuide() {
