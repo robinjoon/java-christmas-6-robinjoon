df4e3fea9edd8cb32b2db1a56bc8eefee9340562
robinjoon <robin980108@naver.com>
2023-11-15 02:41:24 +0900

    refactor(증정 기능) : GiftResults 생성 방식 변경
    LocalDate 객체와 SelectedMenus 객체를 이용해 DiscountResults를 생성하는 메서드는 StatisticsGenerator 내부에 있는 것 보다 DiscountResults 내부에 있는 것이 더 적절하다. 이 메서드를 정적 메서드로 추가하여 SelectedMenus와 독립적으로 동작할 수 있도록 하였다.

--- src/main/java/christmas/domain/discount/DiscountResults.java
+++ src/main/java/christmas/domain/discount/DiscountResults.java
@@ -1,5 +1,8 @@
 package christmas.domain.discount;
 
+import christmas.domain.menu.SelectedMenus;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -8,6 +11,22 @@ public final class DiscountResults {
 
     public DiscountResults(List<DiscountResult> discountResults) {
         this.discountResults = discountResults;
+    }
+
+    public static DiscountResults calculatedFrom(LocalDate selectedDate, SelectedMenus selectedMenus) {
+        List<DiscountResult> discountResults = calculateDiscountResults(selectedDate, selectedMenus);
+        return new DiscountResults(discountResults);
+    }
+
+    private static List<DiscountResult> calculateDiscountResults(LocalDate selectedDate, SelectedMenus selectedMenus) {
+        return Arrays.stream(DiscountType.values())
+                .map(discountType -> calculateDiscountResult(selectedDate, selectedMenus, discountType)).toList();
+    }
+
+    private static DiscountResult calculateDiscountResult(LocalDate selectedDate, SelectedMenus selectedMenus,
+                                                          DiscountType discountType) {
+        DiscountCalculator discountCalculator = DiscountCalculatorMapper.map(discountType).orElseThrow();
+        return discountCalculator.calculate(selectedDate, selectedMenus);
     }
 
     public int getTotalDiscountPrice() {
--- src/main/java/christmas/domain/statistics/StatisticsGenerator.java
+++ src/main/java/christmas/domain/statistics/StatisticsGenerator.java
@@ -2,17 +2,11 @@ package christmas.domain.statistics;
 
 import christmas.domain.badge.Badge;
 import christmas.domain.badge.BadgeCalculator;
-import christmas.domain.discount.DiscountCalculator;
-import christmas.domain.discount.DiscountCalculatorMapper;
-import christmas.domain.discount.DiscountResult;
 import christmas.domain.discount.DiscountResults;
-import christmas.domain.discount.DiscountType;
 import christmas.domain.gift.GiftResults;
 import christmas.domain.menu.SelectedMenus;
 import christmas.domain.menu.SelectedMenusDTO;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
 public final class StatisticsGenerator {
     private final BadgeCalculator badgeCalculator;
@@ -22,21 +16,10 @@ public final class StatisticsGenerator {
     }
 
     public Statistics makeStatistics(LocalDate selectedDate, SelectedMenus selectedMenus) {
-        DiscountResults discountResults = new DiscountResults(calculateDiscountResults(selectedDate, selectedMenus));
+        DiscountResults discountResults = DiscountResults.calculatedFrom(selectedDate, selectedMenus);
         GiftResults giftResults = GiftResults.calculatedFrom(selectedMenus);
         Badge badge = badgeCalculator.calculate(discountResults, giftResults);
         SelectedMenusDTO selectedMenusDTO = selectedMenus.to();
         return new Statistics(selectedDate, selectedMenusDTO, giftResults, discountResults, badge);
-    }
-
-    private static List<DiscountResult> calculateDiscountResults(LocalDate selectedDate, SelectedMenus selectedMenus) {
-        return Arrays.stream(DiscountType.values())
-                .map(discountType -> calculateDiscountResult(selectedDate, selectedMenus, discountType)).toList();
-    }
-
-    private static DiscountResult calculateDiscountResult(LocalDate selectedDate, SelectedMenus selectedMenus,
-                                                          DiscountType discountType) {
-        DiscountCalculator discountCalculator = DiscountCalculatorMapper.map(discountType).orElseThrow();
-        return discountCalculator.calculate(selectedDate, selectedMenus);
     }
 }
