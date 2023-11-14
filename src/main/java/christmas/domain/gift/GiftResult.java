6ce13f6f52704c1863d908e89e44b81d378ec1c0
robinjoon <robin980108@naver.com>
2023-11-14 21:23:22 +0900

    refactor(증정 기능) : GiftResult의 필드 변경
    기존 코드에서 증정품의 가격을 GiftType과 GiftResult가 필드로 가지고 있었다. GiftResult는 GiftType을 이미 필드로 가지므로, GiftResult의 증정품 가격 필드를 제거했다. 이때, 증정품 가격을 기존과 동일하게 GiftResult를 통해 바로 가져 올 수 있도록 giftPrice 메서드를 명시적으로 선언했다.

--- src/main/java/christmas/domain/gift/GiftResult.java
+++ src/main/java/christmas/domain/gift/GiftResult.java
@@ -1,11 +1,18 @@
 package christmas.domain.gift;
 
-public record GiftResult(GiftType giftType, int giftPrice, int giftCount) {
+public record GiftResult(GiftType giftType, int giftCount) {
     public GiftResult(GiftType giftType) {
-        this(giftType, giftType.getPrice(), 1);
+        this(giftType, 1);
     }
 
     public static GiftResult zero(GiftType giftType) {
-        return new GiftResult(giftType, 0, 1);
+        return new GiftResult(giftType, 0);
+    }
+
+    public int giftPrice() {
+        if (giftCount == 0) {
+            return 0;
+        }
+        return giftType.getPrice();
     }
 }
