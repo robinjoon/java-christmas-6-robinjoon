c53ea0da66a3beb475a76fbc9946aff6dd8ce620
robinjoon <robin980108@naver.com>
2023-11-15 02:38:00 +0900

    refactor(증정 기능) : GiftResults 생성 방식 변경
    SelectedMenus 객체를 이용해 GiftResults를 생성하는 메서드는 StatisticsGenerator 내부에 있는 것 보다 GiftResults 내부에 있는 것이 더 적절하다. 이 메서드를 정적 메서드로 추가하여 SelectedMenus와 독립적으로 동작할 수 있도록 하였다.

--- src/main/java/christmas/domain/gift/GiftResults.java
+++ src/main/java/christmas/domain/gift/GiftResults.java
@@ -1,5 +1,7 @@
 package christmas.domain.gift;
 
+import christmas.domain.menu.SelectedMenus;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
@@ -9,6 +11,20 @@ public final class GiftResults {
 
     public GiftResults(List<GiftResult> giftResults) {
         this.giftResults = giftResults;
+    }
+
+    public static GiftResults calculatedFrom(SelectedMenus selectedMenus) {
+        List<GiftResult> giftResults = calculateGiftResults(selectedMenus);
+        return new GiftResults(giftResults);
+    }
+
+    private static List<GiftResult> calculateGiftResults(SelectedMenus selectedMenus) {
+        return Arrays.stream(GiftType.values()).map(giftType -> calculateGift(selectedMenus, giftType)).toList();
+    }
+
+    private static GiftResult calculateGift(SelectedMenus selectedMenus, GiftType giftType) {
+        GiftCalculator giftCalculator = GiftCalculatorMapper.map(giftType).orElseThrow();
+        return giftCalculator.calculate(selectedMenus);
     }
 
     public int getTotalGiftPrice() {
--- src/main/java/christmas/domain/statistics/StatisticsGenerator.java
+++ src/main/java/christmas/domain/statistics/StatisticsGenerator.java
@@ -7,11 +7,7 @@ import christmas.domain.discount.DiscountCalculatorMapper;
 import christmas.domain.discount.DiscountResult;
 import christmas.domain.discount.DiscountResults;
 import christmas.domain.discount.DiscountType;
-import christmas.domain.gift.GiftCalculator;
-import christmas.domain.gift.GiftCalculatorMapper;
-import christmas.domain.gift.GiftResult;
 import christmas.domain.gift.GiftResults;
-import christmas.domain.gift.GiftType;
 import christmas.domain.menu.SelectedMenus;
 import christmas.domain.menu.SelectedMenusDTO;
 import java.time.LocalDate;
@@ -27,19 +23,10 @@ public final class StatisticsGenerator {
 
     public Statistics makeStatistics(LocalDate selectedDate, SelectedMenus selectedMenus) {
         DiscountResults discountResults = new DiscountResults(calculateDiscountResults(selectedDate, selectedMenus));
-        GiftResults giftResults = new GiftResults(calculateGiftResults(selectedMenus));
+        GiftResults giftResults = GiftResults.calculatedFrom(selectedMenus);
         Badge badge = badgeCalculator.calculate(discountResults, giftResults);
         SelectedMenusDTO selectedMenusDTO = selectedMenus.to();
         return new Statistics(selectedDate, selectedMenusDTO, giftResults, discountResults, badge);
-    }
-
-    private static List<GiftResult> calculateGiftResults(SelectedMenus selectedMenus) {
-        return Arrays.stream(GiftType.values()).map(giftType -> calculateGift(selectedMenus, giftType)).toList();
-    }
-
-    private static GiftResult calculateGift(SelectedMenus selectedMenus, GiftType giftType) {
-        GiftCalculator giftCalculator = GiftCalculatorMapper.map(giftType).orElseThrow();
-        return giftCalculator.calculate(selectedMenus);
     }
 
     private static List<DiscountResult> calculateDiscountResults(LocalDate selectedDate, SelectedMenus selectedMenus) {
